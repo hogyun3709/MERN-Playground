06d90df81d8c759b187095d480906f100c8453e6
hogyun3709 <hk421@uowmail.edu.au>
2018-04-18 17:38:24 +0900

    Redux setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}><App/></Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
