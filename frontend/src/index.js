a57e5f1983ae2d54e14fe6c2b9b91d7929b17405
hogyun3709 <hk421@uowmail.edu.au>
2018-04-19 14:00:15 +0900

    Reducer setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import App from './App';
+import reducers from './reducers'
 import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(() => reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}><App/></Provider>,
