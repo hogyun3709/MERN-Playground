b03e8091289d60478d263a5fe0a1bbadc97e9f6c
hogyun3709 <hk421@uowmail.edu.au>
2018-04-21 16:42:02 +0900

    Setup ajax call w/ redux-thunk && add proxy rules for api

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-// import { Button, Icon } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 
 import Header from "./Header";
 const DashBoard = () => <h2>DashBoard</h2>;
@@ -14,9 +14,11 @@ class App extends Component {
         <BrowserRouter>
           <div>
             <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={DashBoard} />
-            <Route path="/surveys/new" component={SurveyNew} />
+            <Container>
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/surveys" component={DashBoard} />
+              <Route path="/surveys/new" component={SurveyNew} />
+            </Container>
           </div>
         </BrowserRouter>
       </div>
@@ -25,31 +27,3 @@ class App extends Component {
 }
 
 export default App;
-
-{
-  /* <header className="App-header">
-  <h1 className="App-title">Welcome to React</h1>
-</header>
-<p className="App-intro">
-  To get started, edit <code>src/App.js</code> and save to reload.
-</p>
-<Button primary>Primary</Button>
-<Button secondary>Secondary</Button>
-<Button animated>
-  <Button.Content visible>Next</Button.Content>
-  <Button.Content hidden>
-    <Icon name="right arrow" />
-  </Button.Content>
-</Button>
-<Button animated="vertical">
-  <Button.Content hidden>Shop</Button.Content>
-  <Button.Content visible>
-    <Icon name="shop" />
-  </Button.Content>
-</Button>
-<Button animated="fade">
-  <Button.Content visible>Sign-up for a Pro account</Button.Content>
-  <Button.Content hidden>$12.99 a month</Button.Content>
-</Button>
-<a href="/auth/google">Sign In with Google</a> */
-}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk'
 import App from './components/App';
 import reducers from './reducers'
 import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(() => reducers, {}, applyMiddleware());
+const store = createStore(() => reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}><App/></Provider>,
