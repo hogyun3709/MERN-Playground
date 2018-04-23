bbb30d2266f843cbd9a0da8e84672717c49462f2
hogyun3709 <hk421@uowmail.edu.au>
2018-04-23 16:21:44 +0900

    Usuage of Link tags

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { connect } from "react-redux";
-import * as actions from '../actions';
+import * as actions from "../actions";
 import { Container } from "semantic-ui-react";
 
 import Header from "./Header";
+import Landing from "./Landing";
+
 const DashBoard = () => <h2>DashBoard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   componentDidMount() {
