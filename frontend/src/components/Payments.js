3d8d359dcdacff4394e6a4c215b8b3ae2b6eff02
hogyun3709 <hk421@uowmail.edu.au>
2018-04-24 22:36:32 +0900

    Hanlding stripe token from client side

--- frontend/src/components/Payments.js
+++ frontend/src/components/Payments.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Button } from "semantic-ui-react";
-import StripeCheckout from 'react-stripe-checkout';
+import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class Payments extends Component {
-  render(){
+  render() {
     return (
       <StripeCheckout
         name="DesignGround"
         description="$5 for 5 credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)} 
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
-        <Button basic color='green'>
+        <Button basic color="green">
           Add Credits
         </Button>
       </StripeCheckout>
     );
   }
 }
-export default Payments;
+export default connect(null, actions)(Payments);
