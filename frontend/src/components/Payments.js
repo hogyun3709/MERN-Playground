0005d6d26a674facd274607d0f379f786b60e685
hogyun3709 <hk421@uowmail.edu.au>
2018-04-24 17:35:30 +0900

    Payment styling

--- frontend/src/components/Payments.js
+++ frontend/src/components/Payments.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Button } from "semantic-ui-react";
 import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
   render(){
     return (
       <StripeCheckout
+        name="DesignGround"
+        description="$5 for 5 credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <Button basic color='green'>
+          Add Credits
+        </Button>
+      </StripeCheckout>
     );
   }
 }
