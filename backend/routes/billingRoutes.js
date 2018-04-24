0964bae808edd757bbe5e0f69fdec4a2969105d3
hogyun3709 <hk421@uowmail.edu.au>
2018-04-24 22:35:10 +0900

    Create Charge object

--- backend/routes/billingRoutes.js
+++ backend/routes/billingRoutes.js
@@ -1,9 +1,13 @@
-const keys = require('../config/key')
-const stripe = require('stripe')(
-  keys.stripeSecretKey
-);
+const keys = require("../config/key");
+const stripe = require("stripe")(keys.stripeSecretKey);
 module.exports = app => {
-  app.post('/api/stripe', (req, res) => {
-
+  app.post("/api/stripe", async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      description: "Service costs $5",
+      source: req.body.id
+    });
+    console.log(charge);
   });
-}
+};
