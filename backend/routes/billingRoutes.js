a1b16961142fa3725f3c075a17dbca3634727e26
hogyun3709 <hk421@uowmail.edu.au>
2018-04-24 23:28:15 +0900

    Build a middleware for user auth check

--- backend/routes/billingRoutes.js
+++ backend/routes/billingRoutes.js
@@ -1,14 +1,16 @@
 const keys = require("../config/key");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 module.exports = app => {
-  app.post("/api/stripe", async (req, res) => {
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
-      currency: "usd",
-      description: "Service costs $5",
+      currency: 'usd',
+      description: '$5 for 5 credits',
       source: req.body.id
     });
-    res.user.credits += 5;
+
+    req.user.credits += 5;
     const user = await req.user.save();
 
     res.send(user);
