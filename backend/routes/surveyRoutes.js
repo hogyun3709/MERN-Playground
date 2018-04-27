9cd582451626d8c0b76d86c19ce1bc2fc82a57aa
hogyun3709 <hk421@uowmail.edu.au>
2018-04-27 12:25:20 +0900

    Create subdocs

--- backend/routes/surveyRoutes.js
+++ backend/routes/surveyRoutes.js
@@ -1,8 +1,21 @@
-const requireLogin = require('../middlewares/requireLogin');
-const requireCredits = require('../middlewares/requireCredits');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+
+const Survey = mongoose.model("surveys");
+
 module.exports = app => {
   // Can put as many as middlewares argus
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
-  })
-}
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({ email })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+  });
+};
