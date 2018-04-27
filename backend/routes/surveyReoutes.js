59b4db8edd19148d6ca6b72e59e7d6cba3438a8f
hogyun3709 <hk421@uowmail.edu.au>
2018-04-27 09:44:18 +0900

    Verify min. credit

--- backend/routes/surveyReoutes.js
+++
@@ -1,7 +0,0 @@
-const requireLogin = require('../middlewares/requireLogin');
-module.exports = app => {
-  // Can put as many as middlewares argus
-  app.post('/api/surveys', requireLogin, (req, res) => {
-
-  })
-}
