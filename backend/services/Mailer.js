348e573031899ea27c04b94bbf07a506b0108517
hogyun3709 <hk421@uowmail.edu.au>
2018-04-28 19:56:26 +0900

    Mailer constructor

--- backend/services/Mailer.js
+++ backend/services/Mailer.js
@@ -1,9 +1,18 @@
 const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
-const keys = require('../config/keys');
+const keys = require('../config/key');
 
 class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content){
+    super();
 
+    this.from_email = new helper.Email('no-reply@designground.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this,recipients = this.formatAddresses(recipients);
+  }
 }
+
+
 
 module.exports = Mailer;
