3e9f3287e0fef0ebd4cde277292ecd202ded7c8b
hogyun3709 <hk421@uowmail.edu.au>
2018-05-04 16:04:56 +0900

    Redux form setup

---
+++ frontend/src/components/surveys/SurveyNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+  render(){
+    return(
+      <div>
+        Hello survey New
+      </div>
+    )
+  }
+}
+
+export default SurveyNew
