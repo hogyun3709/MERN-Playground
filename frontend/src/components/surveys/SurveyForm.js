7c93f39f7e5208e9e7be51349a76b1a8c286fa4c
hogyun3709 <hk421@uowmail.edu.au>
2018-05-04 17:14:14 +0900

    Redux-Form in practice

--- frontend/src/components/surveys/SurveyForm.js
+++ frontend/src/components/surveys/SurveyForm.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import { reduxForm, Field } from "redux-form";
+import { Input, Button } from "semantic-ui-react";
 
 class SurveyForm extends Component {
   render() {
-    return <div>SurveyFrom</div>;
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field type="text" name="surveyTitle" component={Input} />
+          <Button positive type="submit">Submit</Button>
+        </form>
+      </div>
+    );
   }
 }
 
 export default reduxForm({
-  form: 'surveyForm'
+  form: "surveyForm"
 })(SurveyForm);
