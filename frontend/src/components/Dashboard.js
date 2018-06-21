ccdc970c6bc10544129cbc8bcaafdbd9fc3b6f2b
hogyun3709 <hk421@uowmail.edu.au>
2018-06-21 20:22:44 +0900

    Rendering a List of Surveys

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,19 +1,38 @@
-import React from "react";
-import { Button, Icon } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Button, Rail, Sticky, Header, Grid, Divider } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import SurveyList from "./surveys/SurveyList";
 
-const Dashboard = () => {
-  return (
-    <div>
-      <SurveyList />
-      <Button icon>
-        <Link to="/surveys/new">
-          <Icon name="world" />
-        </Link>
-      </Button>
-    </div>
-  );
-};
+class Dashboard extends Component {
+  state = {};
+
+  handleContextRef = contextRef => this.setState({ contextRef });
+  render() {
+    const { contextRef } = this.state;
+
+    return (
+      <div>
+        <Grid centered columns={2}>
+          <Grid.Column>
+            <div ref={this.handleContextRef}>
+              <SurveyList />
+              <Rail position="right">
+                <Sticky onBottom bottomOffset={50} context={contextRef} offset={800} pushing >
+                  {/* <Divider hidden/> */}
+                  <Link to="/surveys/new">
+                    <Button.Group>
+                      <Button basic color="violet" content="Create a Survey" />
+                      <Button basic color="violet" icon="plus" />
+                    </Button.Group>
+                  </Link>
+                </Sticky>
+              </Rail>
+            </div>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
--- frontend/src/components/surveys/SurveyList.js
+++ frontend/src/components/surveys/SurveyList.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchSurveys } from "../../actions";
+import { Grid, Button, Card, Image } from "semantic-ui-react";
 
 class SurveyList extends Component {
   componentDidMount() {
     this.props.fetchSurveys();
   }
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <Card fluid key={survey._id}>
+          <Card.Content>
+            <Card.Header>{survey.title}</Card.Header>
+            <Card.Meta>
+              Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+            </Card.Meta>
+            <Card.Description>{survey.body}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className="ui two buttons">
+              <Button basic color="green">
+                Yes: {survey.yes}
+              </Button>
+              <Button basic color="red">
+                No: {survey.no}
+              </Button>
+            </div>
+          </Card.Content>
+        </Card>
+      );
+    });
+  }
   render() {
-    return <div>SurveyList</div>;
+    return <div>{this.renderSurveys()}</div>;
   }
 }
 
